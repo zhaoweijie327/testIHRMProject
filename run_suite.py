664aaf3785f1811e5b9b70a135de0d4a39bdff03
zhaoweijie327 <396793472@qq.com>
2020-06-11 10:17:56 +0800

    2020-6-11

--- run_suite.py
+++ run_suite.py
@@ -14,7 +14,8 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestIHRMEmployeeParams))
 suite.addTest(unittest.makeSuite(TestIHRMLoginParams))
 # 4 定义测试报告的目录和报告名称
-report_path = app.BASE_DIR + "/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+#report_path = app.BASE_DIR + "/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+report_path = app.BASE_DIR + "/report/ihrm.html"
 # 5 使用HTMLTestRunner_PY3生成测试报告
 with open(report_path, mode='wb') as f:
     # 实例化HTMLTestRunner_PY3
